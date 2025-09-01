60328ae778e9467cd7265137e985e9f8f6883080
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2025-09-01 12:34:46 +0600

    Update RecommendationController.java

--- aiservice/src/main/java/com/arpon007/fitness/aiservice/controller/RecommendationController.java
+++ aiservice/src/main/java/com/arpon007/fitness/aiservice/controller/RecommendationController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/recommandations")
+@RequestMapping("/api/recommendations")
 public class RecommendationController {
     private final RecommentationService recommendationService;
 
