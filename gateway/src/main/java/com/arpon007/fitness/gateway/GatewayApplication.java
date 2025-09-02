cc1912d9ed8cd336b16b16c463ebbd3689858db7
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2025-09-02 22:51:35 +0600

    update

--- gateway/src/main/java/com/arpon007/fitness/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/arpon007/fitness/gateway/GatewayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args); //code by md shazan 
 	}
 
 }
